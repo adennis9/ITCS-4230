6e5b9f31eca3ff7bc99f71647b830d76e7f53682
Andrew Dennis <Andrew@wifi_stu-10-38-80-226.uncc.edu>
2016-04-07 10:52:52 -0400

    Scoreboard populating correctly

---
+++ Prototype/Labyrinth/Assets/Code/PlayerScoreList.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScoreList : MonoBehaviour 
+{
+	public GameObject playerScoreEntryPrefab;
+	ScoreManager scoreManager;
+	int lastChangeCounter;
+
+	public void Start()
+	{
+		scoreManager = GameObject.FindObjectOfType<ScoreManager> ();
+		lastChangeCounter = scoreManager.getChangeCounter ();
+
+	}
+
+	public void Update()
+	{
+
+
+		if (scoreManager.getChangeCounter() == lastChangeCounter)
+			return;
+		
+		while (this.transform.childCount > 0) 
+		{
+			Transform c = this.transform.GetChild (0);
+			c.SetParent (null);
+			Destroy (c.gameObject);
+		}
+
+		string[] names = scoreManager.getPlayerNames ();
+		foreach(string name in names) 
+		{
+			GameObject go = (GameObject)Instantiate (playerScoreEntryPrefab);
+			go.transform.SetParent (this.transform, false);
+			go.transform.Find("userName").GetComponent<Text>().text = name;
+			go.transform.Find("Score").GetComponent<Text>().text = scoreManager.getScore(name).ToString();
+
+
+		}
+	}
+
+}
--- Prototype/Labyrinth/Assets/Code/ScoreManager.cs
+++ Prototype/Labyrinth/Assets/Code/ScoreManager.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ScoreManager : MonoBehaviour 
 {
 	private Dictionary<string, int> scoreBoard;
-
+	int changeCounter = 0;
 	public void Start()
 	{
-		setScore ("xer096", 9001);
+		setScore ("AAD", 9001);
+		setScore ("CRF", 10000);
+		setScore ("AAA", 8000);
+		setScore ("BBB", 8001);
+		setScore ("CCC", 9000);
 
-		Debug.Log (getScore("xer096"));
+		//Debug.Log (getScore("xer096"));
 	}
 
 	private void init()
@@ -35,11 +40,23 @@ public class ScoreManager : MonoBehaviour
 	public void setScore(string userName, int score)
 	{
 		init ();
-
+		changeCounter++;
 		if (scoreBoard.ContainsKey (userName) == false)
 			scoreBoard.Add(userName, score);
 		else 
 			scoreBoard [userName] = score;
+	}
+
+	public string[] getPlayerNames()
+	{
+		init ();
+
+		return scoreBoard.Keys.OrderByDescending(n => getScore(n)).ToArray();
+	}
+
+	public int getChangeCounter()
+	{
+		return changeCounter;
 	}
 
 
